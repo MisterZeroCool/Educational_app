91440255959b7523ae0648c675fbb3ed86f0ebb7
zerocool <one1970zero@mail.ru>
2023-07-29 15:47:14 +0300

    Добавление активити, добавление дата класса

--- app/src/main/java/ru/zerocool/Easy.kt
+++ app/src/main/java/ru/zerocool/Easy.kt
@@ -1,26 +1,25 @@
 package ru.zerocool
 
-import java.lang.ArithmeticException
 import kotlin.random.Random
 
 object Easy {
     private var answer: Int = 0
 
-    fun getQuestions():Pair<String, Int>{
-        val num1 = Random.nextInt(-100, 100)
-        val num2 = Random.nextInt(-100, 100)
+    fun getQuestions(): Pair<String, Int> {
+        val num1 = Random.nextInt(- 100, 100)
+        val num2 = Random.nextInt(- 100, 100)
         val operator = arrayOf("+", "-", "x", "/").random()
 
         val problem = "$num1 $operator $num2"
 
         answer = try {
-            when(operator){
+            when (operator) {
                 "+" -> num1 + num2
                 "-" -> num1 - num2
                 "x" -> num1 * num2
                 else -> num1 / num2
             }
-        }catch (e: ArithmeticException){
+        } catch (e: ArithmeticException) {
             77777777
         }
 
--- app/src/main/java/ru/zerocool/Medium.kt
+++ app/src/main/java/ru/zerocool/Medium.kt
@@ -17,26 +17,26 @@ object Medium {
         return ans
     }
 
-    fun getQuestions():Pair<String, Int>{
-        val num1 = Random.nextInt(1,100)
-        val num2 = Random.nextInt(1,100)
-        val num3 = Random.nextInt(1,100)
+    fun getQuestions(): Pair<String, Int> {
+        val num1 = Random.nextInt(1, 100)
+        val num2 = Random.nextInt(1, 100)
+        val num3 = Random.nextInt(1, 100)
 
-        val bracketPosition = Random.nextInt(1,3)
+        val bracketPosition = Random.nextInt(1, 3)
         val operator1 = arrayOf("+", "-", "x", "/").random()
         val operator2 = arrayOf("+", "-", "x", "/").random()
 
-        val problem = when(bracketPosition){
+        val problem = when (bracketPosition) {
             1 -> "($num1 $operator1 $num2) $operator2 $num3"
             else -> "$num1 $operator1 ($num2 $operator2 $num3)"
         }
 
-        val halfAns = when(bracketPosition){
+        val halfAns = when (bracketPosition) {
             1 -> makeOperation(num1, num2, operator1)
             else -> makeOperation(num2, num3, operator2)
         }
 
-        val answer = when(bracketPosition){
+        val answer = when (bracketPosition) {
             1 -> makeOperation(halfAns, num3, operator2)
             else -> makeOperation(num1, halfAns, operator1)
         }
